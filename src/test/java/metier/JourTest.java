d29188f694a8a29a5feb4416b5f9a229e5451a59
92930 <92930@192.168.0.16>
2021-03-09 09:39:05 +0100

    3.9

---
+++ src/test/java/metier/JourTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author 92930
+ */
+public class JourTest {
+    
+    public JourTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
